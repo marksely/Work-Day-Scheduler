a152cba5c0c43abc21a9211fe3cea292d3abeb0a
Mark Selyuzhitskiy <mark.sel9@gmail.com>
2021-09-24 18:35:20 -0400

    added local storage to 9am'

--- javascript.js
+++ javascript.js
@@ -1,8 +1,29 @@
 var date = $('#currentDay');
+var eventDetails = $('#event');
+var saveButton = $('.saveBtn');
+var first = $('9am');
 
+//DATE
 function showDate() {
     var currentDay = moment().format('dddd MMMM Do');
     date.text(currentDay);
 }
 
 setInterval(showDate, 0);
+
+//SAVE EVENT TO LOCAL STORAGE
+saveButton.on('click', function(event) {
+
+    event.preventDefault();
+
+    var thingOne = {
+        comment: eventDetails.val()
+    }
+    console.log(thingOne);
+
+    localStorage.setItem('9am', JSON.stringify(thingOne));
+    
+    window.alert('Event has been saved to local storage');
+    getNine();
+});
+
